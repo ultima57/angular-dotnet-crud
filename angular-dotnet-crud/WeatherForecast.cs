377b1ca5b38baef257a07e58dfa13613fd230a8d
ultima57 <artyomzar83@gmail.com>
2023-04-05 13:53:42 +0300

    new branch

--- angular-dotnet-crud/WeatherForecast.cs
+++ angular-dotnet-crud/WeatherForecast.cs
@@ -6,7 +6,7 @@ namespace angular_dotnet_crud
 
         public int TemperatureC { get; set; }
 
-        public int TemperatureF => 33 + (int)(TemperatureC / 0.5556);
+        public int TemperatureF => 303 + (int)(TemperatureC / 0.5556);
 
         public string? Summary { get; set; }
     }
